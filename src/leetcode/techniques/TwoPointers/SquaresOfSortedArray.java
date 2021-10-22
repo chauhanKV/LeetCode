aa54a4d231d9dc302e9a5863866a235082b2433d
KavitaChauhan29 <chauhan.kavita@hotmail.com>
2021-10-22 13:14:22 +0530

    977. Squares of a Sorted Array - Time and Space complexity

--- src/leetcode/techniques/TwoPointers/SquaresOfSortedArray.java
+++ src/leetcode/techniques/TwoPointers/SquaresOfSortedArray.java
@@ -1,6 +1,14 @@
 package leetcode.techniques.TwoPointers;
 
 public class SquaresOfSortedArray {
+    //Runtime: 2 ms, faster than 69.05% of Java online submissions for Squares of a Sorted Array.
+    //Memory Usage: 53.6 MB, less than 6.00% of Java online submissions for Squares of a Sorted Array.
+    //Next challenges:
+    //Merge Sorted Array
+    //Sort Transformed Array
+
+    // Time Complexity : O(n) - one pass
+    // Space Complexity : O(n) - storing sorted array values after squaring
     public int[] sortedSquares(int[] nums) {
         int sortedIndex = nums.length - 1, left = 0, right = nums.length - 1;
         int[] sortedArray = new int[nums.length];
