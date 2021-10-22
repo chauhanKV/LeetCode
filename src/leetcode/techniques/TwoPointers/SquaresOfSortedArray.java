811e6f598a0f293cf547df9a08260648de265af3
KavitaChauhan29 <chauhan.kavita@hotmail.com>
2021-10-22 13:17:40 +0530

    977. Squares of a Sorted Array - More faster and less memory storage

--- src/leetcode/techniques/TwoPointers/SquaresOfSortedArray.java
+++ src/leetcode/techniques/TwoPointers/SquaresOfSortedArray.java
@@ -9,14 +9,22 @@ public class SquaresOfSortedArray {
 
     // Time Complexity : O(n) - one pass
     // Space Complexity : O(n) - storing sorted array values after squaring
+
+    // After declaring leftSquare and rightSquare in first line and not inside while loop -
+    //Runtime: 1 ms, faster than 100.00% of Java online submissions for Squares of a Sorted Array.
+    //Memory Usage: 40.8 MB, less than 77.78% of Java online submissions for Squares of a Sorted Array.
+
     public int[] sortedSquares(int[] nums) {
-        int sortedIndex = nums.length - 1, left = 0, right = nums.length - 1;
+        // int sortedIndex = nums.length - 1, left = 0, right = nums.length - 1;
+        int sortedIndex = nums.length - 1, left = 0, right = nums.length - 1, leftSquare = 0, rightSquare = 0;
         int[] sortedArray = new int[nums.length];
 
         while(left <= right)
         {
-            int leftSquare = nums[left] * nums[left];
-            int rightSquare = nums[right] * nums[right];
+            // int leftSquare = nums[left] * nums[left];
+            // int rightSquare = nums[right] * nums[right];
+            leftSquare = nums[left] * nums[left];
+            rightSquare = nums[right] * nums[right];
 
             if(leftSquare > rightSquare)
             {
