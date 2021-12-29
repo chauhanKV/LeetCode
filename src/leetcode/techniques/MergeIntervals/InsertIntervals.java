f92c2844bc46fd5d20ae3f31b04803b9e97403e3
KavitaChauhan29 <chauhan.kavita@hotmail.com>
2021-12-29 09:14:32 +0530

    57. Insert Intervals - Optimzed Solution (doesn't work when there are overlapping intervals in the question)

--- src/leetcode/techniques/MergeIntervals/InsertIntervals.java
+++ src/leetcode/techniques/MergeIntervals/InsertIntervals.java
@@ -1,19 +1,46 @@
 package leetcode.techniques.MergeIntervals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InsertIntervals {
     public int[][] insert(int[][] intervals, int[] newInterval) {
 
         if(intervals.length <= 0) return intervals;
         if(newInterval.length <= 0) return intervals;
 
-        for(int i = 0; i < intervals.length; i++)
+        List<int[]> result = new ArrayList<>();
+        int i = 0, n = intervals.length;
+
+        // First case
+        // when end of current interval is less than start of new Interval
+        while(i < n && intervals[i][1] < newInterval[0])
         {
-            if(intervals[i][1] > newInterval[0])
-            {
-                intervals[i][1] = Math.max(intervals[i][1], newInterval[1]);
-            }
+            result.add(intervals[i]);
+            i++;
         }
 
-        return intervals;
+        // Second case
+        // 1. when end of the current is greater than start of new interval -> above while loop will break at this point (This condition is already checked in the negative case of above while loop.)
+        // 2. when start of current is less than end of new Interval
+        int[] mergedInterval = new int[2];
+        while(i < n && intervals[i][0] < newInterval[1])
+        {
+            mergedInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            mergedInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+        result.add(mergedInterval);
+
+
+        // Third case
+        // Add all non overlapping elements
+        while(i < n)
+        {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        return result.toArray(new int[result.size()][2]);
     }
 }
