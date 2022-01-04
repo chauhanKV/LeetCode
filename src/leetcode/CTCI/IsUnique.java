63d612c802e4303c06d2295780852ecd05e6c649
KavitaChauhan29 <chauhan.kavita@hotmail.com>
2022-01-04 21:18:25 +0530

    CTCI 1.1 IsUnique

---
+++ src/leetcode/CTCI/IsUnique.java
@@ -0,0 +1,22 @@
+package leetcode.CTCI;
+
+public class IsUnique {
+    public boolean isUnique(String value)
+    {
+        if(value.length() > 128)
+            return false;
+        // Space of 128 is used
+        boolean[] arr = new boolean[128]; // boolean declares to false by default
+        for(int i = 0 ; i < value.length(); i++)
+        {
+            boolean charStatus = arr[value.charAt(i) - 'a'];
+            if(charStatus)
+            {
+                return !charStatus;
+            }
+            arr[value.charAt(i) - 'a'] = true;
+        }
+        return true;
+    }
+
+}
---
+++ src/leetcode/CTCI/Main.java
@@ -0,0 +1,10 @@
+package leetcode.CTCI;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        IsUnique isUnique = new IsUnique();
+        System.out.println("Is unique character ? : " + isUnique.isUnique("character"));
+    }
+}
