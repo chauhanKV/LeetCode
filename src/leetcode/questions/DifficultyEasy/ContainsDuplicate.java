d8187324e63e3bcf9b72df947edd5398463f178b
KavitaChauhan29 <chauhan.kavita@hotmail.com>
2022-01-04 23:43:58 +0530

    217. Contains Duplicate - With HashSet (Not Optimal)

--- src/leetcode/questions/DifficultyEasy/ContainsDuplicate.java
+++ src/leetcode/questions/DifficultyEasy/ContainsDuplicate.java
@@ -1,6 +1,7 @@
 package leetcode.questions.DifficultyEasy;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class ContainsDuplicate {
     //Runtime: 16 ms, faster than 10.46% of Java online submissions for Contains Duplicate.
@@ -25,4 +26,29 @@ public class ContainsDuplicate {
         }
         return false;
     }
+
+    //Runtime: 6 ms, faster than 68.25% of Java online submissions for Contains Duplicate.
+    //Memory Usage: 52.1 MB, less than 28.07% of Java online submissions for Contains Duplicate.
+    //Next challenges:
+    //Contains Duplicate III
+
+    // Time complexity : O(N)
+    // Space Complexity : O(N)
+
+//    public boolean containsDuplicate(int[] nums) {
+//        HashSet<Integer> set = new HashSet<>();
+//
+//        for(int i = 0 ; i < nums.length; i++)
+//        {
+//            if(set.contains(nums[i]))
+//            {
+//                return true;
+//            }
+//            else
+//            {
+//                set.add(nums[i]);
+//            }
+//        }
+//        return false;
+//    }
 }
