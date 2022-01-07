b77d6f4f1df843b43922abecef0a4898ed57ed86
KavitaChauhan29 <chauhan.kavita@hotmail.com>
2022-01-07 09:46:39 +0530

    231. Power of Two

---
+++ src/leetcode/techniques/BitManipulation/Main.java
@@ -0,0 +1,10 @@
+package leetcode.techniques.BitManipulation;
+
+public class Main {
+    public static void main (String[] args)
+    {
+        isPowerOfTwo powerOfTwo = new isPowerOfTwo();
+        boolean power = powerOfTwo.isPowerOfTwo(30);
+        System.out.println("Is power of two : " + power);
+    }
+}
