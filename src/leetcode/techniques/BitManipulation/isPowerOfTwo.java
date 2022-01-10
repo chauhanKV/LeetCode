57c392060daeb1881da325268dd592310cadc411
KavitaChauhan29 <chauhan.kavita@hotmail.com>
2022-01-10 09:26:02 +0530

    231. Power of Two | Optimized approach

--- src/leetcode/techniques/BitManipulation/isPowerOfTwo.java
+++ src/leetcode/techniques/BitManipulation/isPowerOfTwo.java
@@ -1,8 +1,15 @@
 package leetcode.techniques.BitManipulation;
 
 public class isPowerOfTwo {
+    //Runtime: 1 ms, faster than 99.97% of Java online submissions for Power of Two.
+    //Memory Usage: 35.9 MB, less than 65.65% of Java online submissions for Power of Two.
+    //Next challenges:
+    //Power of Three
+    //Power of Four
+
+    // Time Complexity : O(N)
+    // Space Complexity : O(1)
     public boolean isPowerOfTwo(int n) {
-        if(n == 0) return false;
-        return (n & (n-1)) == 0;
+        return n > 0 && ((n & (n-1)) == 0);
     }
 }
