1709f7170244963d7e38742ff5a78345eda304a7
KavitaChauhan29 <chauhan.kavita@hotmail.com>
2022-01-20 21:20:24 +0530

    204. Count Primes - Sieve of Eratosthenes - Optimized Solution

--- src/leetcode/techniques/NumberTheory/CountPrimeNumbers.java
+++ src/leetcode/techniques/NumberTheory/CountPrimeNumbers.java
@@ -2,38 +2,52 @@ package leetcode.techniques.NumberTheory;
 
 public class CountPrimeNumbers {
 
-    // Sieve of Eratosthenes
-    // Understand more clearly.
-    public int countPrimes(int n) {
-        if(n == 0) return 0;
-        boolean isPrime[] = new boolean[n+1];
-        int countPrime = 0;
 
-        for(int i = 0 ; i < isPrime.length; i++)
-        {
+    // Sieve of Eratosthenes
+
+    //Runtime: 245 ms, faster than 16.32% of Java online submissions for Count Primes.
+    //Memory Usage: 65.2 MB, less than 37.01% of Java online submissions for Count Primes.
+    //Next challenges:
+    //Ugly Number
+    //Ugly Number II
+    //Perfect Squares
+
+    // Time Complexity -
+    // Space Complexity -
+    public int countPrimes(int n) {
+        // Create boolean array with size n
+        boolean[] isPrime = new boolean[n];
+
+        // Fill all values from 2 index as true; consider all prime initially.
+        for (int i = 2; i < n; i++) {
             isPrime[i] = true;
         }
-        isPrime[0] = false;
-        isPrime[1] = false;
 
-        for(int i = 2; i * i <= n ; i++)
+        int primeCounter = 0;
+
+        // run the loop till root of n or while coding -> i * i < n as we cannot create square root function
+        for(int i = 2; i * i < isPrime.length; i++)
         {
+            // Run below code if we that index is not marked false;
             if(isPrime[i] == true)
             {
-                for(int j = i * i; j <= n; j = j+i)
+                // initialize div to square of i and let div jump in multiples of i
+                for(int div = i * i; div < isPrime.length; div += i)
                 {
-                    isPrime[j] = false;
+                    isPrime[div] = false;
                 }
             }
         }
 
-        for(int i = 0 ; i < isPrime.length; i++)
+        for(int i = 2; i < isPrime.length; i++)
         {
-            if(isPrime[i] == true)
+            if(isPrime[i])
             {
-                countPrime++;
+                primeCounter++;
             }
         }
-        return countPrime;
+
+
+        return primeCounter;
     }
 }
--- src/leetcode/techniques/NumberTheory/Main.java
+++ src/leetcode/techniques/NumberTheory/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String args[])
     {
         CountPrimeNumbers count = new CountPrimeNumbers();
-        int primeCount = count.countPrimes(4);
+        int primeCount = count.countPrimes(50);
         System.out.println("Count of prime number are : " + primeCount);
     }
 }
