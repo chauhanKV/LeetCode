3a5af83ad1efb173fcebb3bad8979f6152612b6b
KavitaChauhan29 <chauhan.kavita@hotmail.com>
2022-01-31 08:51:18 +0530

    242. Valid Anagram - Optimized solution using sorting

--- src/leetcode/questions/DifficultyEasy/ValidAnagrams.java
+++ src/leetcode/questions/DifficultyEasy/ValidAnagrams.java
@@ -1,5 +1,7 @@
 package leetcode.questions.DifficultyEasy;
 
+import java.util.Arrays;
+
 public class ValidAnagrams {
     public boolean isAnagram(String s, String t) {
         int[] map = new int[26];
@@ -19,5 +21,26 @@ public class ValidAnagrams {
         }
 
         return true;
+    }
+
+    //Runtime: 2 ms, faster than 97.17% of Java online submissions for Valid Anagram.
+    //Memory Usage: 42.4 MB, less than 18.79% of Java online submissions for Valid Anagram.
+    //Next challenges:
+    //Group Anagrams
+    //Palindrome Permutation
+
+    // Time Complexity : O(log n)
+    // Space Complexity : O(N)
+    public boolean isAnagramOptimized(String s, String t) {
+        if(s.length() <= 0 || t.length() <= 0) return false;
+        if(s.length() != t.length()) return false;
+
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        return Arrays.equals(sArray, tArray);
     }
 }
