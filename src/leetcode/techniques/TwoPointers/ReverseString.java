d86520ff214198af87988e69d84029645e8805ed
KavitaChauhan29 <chauhan.kavita@hotmail.com>
2022-02-02 09:40:07 +0530

    344. Reverse String - Two pointers working solution.

--- src/leetcode/techniques/TwoPointers/ReverseString.java
+++ src/leetcode/techniques/TwoPointers/ReverseString.java
@@ -1,10 +1,18 @@
 package leetcode.techniques.TwoPointers;
 
 public class ReverseString {
+    //Runtime: 6 ms, faster than 5.91% of Java online submissions for Reverse String.
+    //Memory Usage: 57.2 MB, less than 5.37% of Java online submissions for Reverse String.
+    //Next challenges:
+    //Reverse Vowels of a String
+    //Reverse String II
+
+    // Time Complexity : O(Log N)
+    // Space Complexity : O(1)
     public void reverseString(char[] s) {
         int start = 0, end = s.length - 1;
 
-        while(start != end && start <= s.length - 1 && end >= 0)
+        while(start < end && start <= s.length - 1 && end >= 0)
         {
             char temp = s[start];
             s[start] = s[end];
