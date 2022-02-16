38a11f81bdd148386bc1a852d40c4018739b9db3
KavitaChauhan29 <chauhan.kavita@hotmail.com>
2022-02-16 09:34:06 +0530

    69. Sqrt(x) - Binary search approach

--- src/leetcode/techniques/NumberTheory/Sqrt.java
+++ src/leetcode/techniques/NumberTheory/Sqrt.java
@@ -5,67 +5,103 @@ import java.util.Stack;
 
 public class Sqrt {
 
-    // Using Long division method - Not a good approach
-    // Need to check Newton Raphson method to calculate square root
-    public int findSqrt(int n) {
+    // Runtime: 2 ms, faster than 69.03% of Java online submissions for Sqrt(x).
+    //Memory Usage: 41.7 MB, less than 9.83% of Java online submissions for Sqrt(x).
+    //Next challenges:
+    //Pow(x, n)
+    //Valid Perfect Square
 
-        ArrayList<Integer> pairs = new ArrayList<>();
-        int digitCnt = 0;
-        //create pairs of given number, store them in list or array
-        while (n != 0) {
-            digitCnt = digitCount(n);
-            if (digitCnt > 1) {
-                pairs.add(n % 100);
-                n = n / 100;
-            } else {
-                pairs.add(n);
-                n = n / 10;
-            }
-        }
-        // loop through the pairs until we reach the end
-        int dividend = pairs.get(pairs.size() - 1), remainder = 0, divisor = 0, divisorTemp = 0, quotient = 0, sqRoot = 0;
+    // Time Complexity : O(LogN)
+    // Space Complexity : O(1)
+    public int mySqrt(int n) {
+        if(n == 0) return 0;
 
-        for(int i = pairs.size() - 1; i >= 0; i--) {
+        int start = 1, end = n;
 
-            divisor = 2 * divisor;
-            divisor = divisor * 10;
-            divisorTemp = divisor;
-            int counter = 0;
-            quotient = counter;
-
-            while (divisor * quotient <= dividend) {
-                ++counter;
-                divisor = divisorTemp + counter;
-                quotient = counter;
-            }
-
-            if(divisor * quotient > dividend) {
-                quotient = quotient - 1;
-                divisor = divisor - 1;
-            }
-
-            remainder = dividend - (quotient * divisor);
-
-            sqRoot = (sqRoot * 10) + quotient;
-
-            if((i - 1) < 0)
+        while(start < end)
+        {
+            int middle = start + (end - start) / 2;
+            if(middle <= n/middle && (middle + 1) > n/(middle + 1))
             {
-                break;
+                return middle;
+            }
+            else if(middle > n/middle)
+            {
+                end = middle;
+            }
+            else
+            {
+                start = middle + 1;
             }
 
-            dividend = pairs.get(i - 1);
-            remainder = remainder * (int) Math.pow(10, digitCount(dividend));
-            dividend = dividend + remainder;
         }
-        return sqRoot;
+        return start;
+
     }
 
-    private int digitCount(int n) {
-        int counter = 1;
-        while (n != 0) {
-            n = n / 10;
-            counter++;
-        }
-        return counter;
-    }
+
+
+
+    // Using Long division method - Not a good approach
+//    public int findSqrt(int n) {
+//
+//        ArrayList<Integer> pairs = new ArrayList<>();
+//        int digitCnt = 0;
+//        //create pairs of given number, store them in list or array
+//        while (n != 0) {
+//            digitCnt = digitCount(n);
+//            if (digitCnt > 1) {
+//                pairs.add(n % 100);
+//                n = n / 100;
+//            } else {
+//                pairs.add(n);
+//                n = n / 10;
+//            }
+//        }
+//        // loop through the pairs until we reach the end
+//        int dividend = pairs.get(pairs.size() - 1), remainder = 0, divisor = 0, divisorTemp = 0, quotient = 0, sqRoot = 0;
+//
+//        for(int i = pairs.size() - 1; i >= 0; i--) {
+//
+//            divisor = 2 * divisor;
+//            divisor = divisor * 10;
+//            divisorTemp = divisor;
+//            int counter = 0;
+//            quotient = counter;
+//
+//            while (divisor * quotient <= dividend) {
+//                ++counter;
+//                divisor = divisorTemp + counter;
+//                quotient = counter;
+//            }
+//
+//            if(divisor * quotient > dividend) {
+//                quotient = quotient - 1;
+//                divisor = divisor - 1;
+//            }
+//
+//            remainder = dividend - (quotient * divisor);
+//
+//            sqRoot = (sqRoot * 10) + quotient;
+//
+//            if((i - 1) < 0)
+//            {
+//                break;
+//            }
+//
+//            dividend = pairs.get(i - 1);
+//            remainder = remainder * (int) Math.pow(10, digitCount(dividend));
+//            dividend = dividend + remainder;
+//        }
+//        return sqRoot;
+//    }
+//
+//    private int digitCount(int n) {
+//        int counter = 1;
+//        while (n != 0) {
+//            n = n / 10;
+//            counter++;
+//        }
+//        return counter;
+//    }
 }
