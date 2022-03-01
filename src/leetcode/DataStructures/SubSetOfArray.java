ef58149b3dc6631dbb008cc6a75bf2df7f9a258b
KavitaChauhan29 <chauhan.kavita@hotmail.com>
2022-03-01 20:50:55 +0530

    Nados - Functions and Arrays - Print SubSet of an Array / Power Set

---
+++ src/leetcode/DataStructures/SubSetOfArray.java
@@ -0,0 +1,28 @@
+package leetcode.DataStructures;
+
+public class SubSetOfArray {
+    public void printSubsetOfArray(int[] arr)
+    {
+        int limit = (int)Math.pow(2, arr.length);
+        for(int i = 0 ; i < limit; i++)
+        {
+            int temp = i;
+            String set = "";
+            for(int j = arr.length - 1; j >= 0; j--)
+            {
+                int remainder = temp % 2;
+                temp = temp / 2;
+
+                if(remainder == 0)
+                {
+                    set = "-" + "\t" + set;
+                }
+                else
+                {
+                    set = arr[j] + "\t" + set;
+                }
+            }
+            System.out.println(set);
+        }
+    }
+}
