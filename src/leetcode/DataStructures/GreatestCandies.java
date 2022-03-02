e5228bcef4f5180a93b2f8b6f7d3905dfd69ce8f
KavitaChauhan29 <chauhan.kavita@hotmail.com>
2022-03-02 20:30:57 +0530

    1431. Kids With the Greatest Number of Candies

---
+++ src/leetcode/DataStructures/GreatestCandies.java
@@ -0,0 +1,33 @@
+package leetcode.DataStructures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GreatestCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+        List<Boolean> result = new ArrayList<>();
+
+        for(int i = 0 ; i < candies.length; i++)
+        {
+            if(max < candies[i])
+            {
+                max = candies[i];
+            }
+        }
+
+        for(int i = 0 ; i < candies.length; i++)
+        {
+            if(candies[i] + extraCandies >= max)
+            {
+                result.add(true);
+            }
+            else
+            {
+                result.add(false);
+            }
+        }
+
+        return result;
+    }
+}
