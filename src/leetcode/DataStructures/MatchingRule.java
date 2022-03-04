971861326490d28449ecc7de231052a3b9f5527e
KavitaChauhan29 <chauhan.kavita@hotmail.com>
2022-03-04 19:42:47 +0530

    1773. Count Items Matching a Rule

---
+++ src/leetcode/DataStructures/MatchingRule.java
@@ -0,0 +1,33 @@
+package leetcode.DataStructures;
+
+import java.util.List;
+
+public class MatchingRule {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int count = 0;
+        for (List<String> products : items) {
+            int rule = 0;
+            for (String val : products) {
+
+                if (ruleKey.equals("type") && rule == 0) {
+                    if (ruleValue.equals(val)) {
+                        count++;
+                    }
+                }
+                if (ruleKey.equals("color") && rule == 1) {
+                    if (ruleValue.equals(val)) {
+                        count++;
+                    }
+                }
+                if (ruleKey.equals("name") && rule == 2) {
+                    if (ruleValue.equals(val)) {
+                        count++;
+                    }
+                }
+                rule++;
+
+            }
+        }
+        return count;
+    }
+}
