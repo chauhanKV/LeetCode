17974b5a0199fc435d22cffb43808ff5cf3c0a0e
KavitaChauhan29 <chauhan.kavita@hotmail.com>
2022-03-29 12:12:14 +0530

    Nados - Recursion and Backtracking - Power-linear

--- src/leetcode/techniques/Recursion/Main.java
+++ src/leetcode/techniques/Recursion/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         BinarySearchWithRecursion recursionSearch = new BinarySearchWithRecursion();
         System.out.println("Result after binary search using recursion is : " + recursionSearch.search(new int[] {-1,0,3,5,9,12}, 2));
+
+        PowerLinear pow = new PowerLinear();
+        System.out.println("Power function : " + pow.power(2, 9));
     }
 }
---
+++ src/leetcode/techniques/Recursion/PowerLinear.java
@@ -0,0 +1,14 @@
+package leetcode.techniques.Recursion;
+
+public class PowerLinear {
+    public int power(int x, int n){
+        if(n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return x * power(x, n - 1);
+        }
+    }
+}
