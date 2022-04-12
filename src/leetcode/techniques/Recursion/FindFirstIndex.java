e115284f94850e222b6f14c69bdc9177b27c8258
Kavita <chauhan.kavita@hotmail.com>
2022-04-12 15:33:48 +0530

    Nados - Recursion and Backtracking for Beginners - Find First Index

---
+++ src/leetcode/techniques/Recursion/FindFirstIndex.java
@@ -0,0 +1,21 @@
+package leetcode.techniques.Recursion;
+
+public class FindFirstIndex {
+    public int firstIndex(int[] arr, int idx, int x){
+
+        if(idx == arr.length)
+        {
+            return -1;
+        }
+
+        if(arr[idx] == x)
+        {
+            return idx;
+        }
+        else
+        {
+            return firstIndex(arr, idx+1, x);
+        }
+
+    }
+}
