7ea2b98391459678fe571bbc0be8065409088513
kavitachauhan <chauhan.kavita@hotmail.com>
2022-07-26 10:21:29 +0530

    51. N-Queens - Super Optimized using int array

--- src/leetcode/techniques/Recursion/NQueens.java
+++ src/leetcode/techniques/Recursion/NQueens.java
@@ -89,4 +89,56 @@ public class NQueens {
         return true;
     }
 
+
+
+    /* Most optimized approach - to not iterate over backwards, lower diagonally or upper diagonally. */
+    //Runtime: 2 ms, faster than 97.74% of Java online submissions for N-Queens.
+    //Memory Usage: 45.4 MB, less than 50.09% of Java online submissions for N-Queens.
+    //Next challenges:
+    //Grid Illumination
+
+    public List<List<String>> solveNQueensOptimized(int n) {
+        char[][] chessboard = new char[n][n];
+
+        for (int i = 0; i < chessboard.length; i++) {
+            for (int j = 0; j < chessboard[0].length; j++) {
+                chessboard[i][j] = '.';
+            }
+        }
+
+        int[] backwardQueens = new int[n];
+        int[] lowerDiagonalQueens = new int[2 * n - 1];
+        int[] upperDiagonalQueens = new int[2 * n - 1];
+
+        List<List<String>> placedQueens = new ArrayList<>();
+        solveNQueensHelperOptimized(chessboard, placedQueens, 0, n, backwardQueens, lowerDiagonalQueens, upperDiagonalQueens);
+        return placedQueens;
+    }
+
+    public void solveNQueensHelperOptimized(char[][] chessboard, List<List<String>> placedQueens, int col, int n, int[] backwardQueens, int[] lowerDiagonalQueens, int[] upperDiagonalQueens) {
+        // base condition
+        if (col == n) {
+            placedQueens.add(convertToList(chessboard));
+            return;
+        }
+
+        for (int row = 0; row < n; row++) {
+            if (backwardQueens[row] != 1 && lowerDiagonalQueens[row + col] != 1 && upperDiagonalQueens[(n - 1) + (col - row)] != 1) // Is safe to place the queen at this location?
+            {
+                chessboard[row][col] = 'Q';
+
+                backwardQueens[row] = 1;
+                lowerDiagonalQueens[row + col] = 1;
+                upperDiagonalQueens[(n - 1) + (col - row)] = 1;
+
+                solveNQueensHelperOptimized(chessboard, placedQueens, col + 1, n, backwardQueens, lowerDiagonalQueens, upperDiagonalQueens);
+                chessboard[row][col] = '.';
+
+                backwardQueens[row] = 0;
+                lowerDiagonalQueens[row + col] = 0;
+                upperDiagonalQueens[(n - 1) + (col - row)] = 0;
+            }
+        }
+    }
+
 }
