06e3567a68396e1257f40ec330ad84780255d7bd
kavitachauhan <chauhan.kavita@hotmail.com>
2022-08-15 08:57:54 +0530

    132. Palindrome Partitioning II - Optimized Solution

--- src/leetcode/techniques/Recursion/PalindromePartitionII.java
+++ src/leetcode/techniques/Recursion/PalindromePartitionII.java
@@ -3,8 +3,6 @@ package leetcode.techniques.Recursion;
 public class PalindromePartitionII {
 
     // This solution give TLE. Will have to use DP to store the result so that my logic doesn't have to compute. Come to this problem once you learn DP.
-    // TC :
-    // SC :
 
     public int minCut(String s) {
         return minCutHelper(s, 0, 0, Integer.MAX_VALUE);
@@ -35,5 +33,48 @@ public class PalindromePartitionII {
         }
 
         return true;
+    }
+
+
+    //Optimized solution using DP and MCM
+    //Runtime: 1329 ms, faster than 32.83% of Java online submissions for Palindrome Partitioning II.
+    //Memory Usage: 55.3 MB, less than 24.40% of Java online submissions for Palindrome Partitioning II.
+    //Next challenges:
+    //Palindrome Partitioning IV
+
+    public int minCutOptimized(String s) {
+        int[][] dp = new int[s.length()][s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < s.length(); j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        return minCutHelperOptimized(s, 0, s.length() - 1, Integer.MAX_VALUE, dp);
+    }
+
+    public int minCutHelperOptimized(String s, int start, int end, int minCut, int[][] dp) {
+        // base condition
+        if (start >= end) {
+            return 0;
+        }
+
+        if (isPalindrome(s, start, end)) {
+            return 0;
+        }
+
+        if (dp[start][end] != -1) {
+            return dp[start][end];
+        }
+
+        for (int i = start; i < end; i++) {
+            if (isPalindrome(s, start, i)) {
+                minCut = Math.min(1 + minCutHelperOptimized(s, i + 1, end, minCut, dp), minCut);
+            }
+        }
+
+        dp[start][end] = minCut;
+        return minCut;
     }
 }
