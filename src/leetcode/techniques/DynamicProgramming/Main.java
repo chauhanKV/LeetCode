bf24376befde20cf30181adc216f19ccf6c5b9ae
kavitachauhan <chauhan.kavita@hotmail.com>
2022-10-27 08:55:45 +0530

    Dynamic Programming by Aditya Verma - 0/1 Knapsack Problem Memoization

--- src/leetcode/techniques/DynamicProgramming/Main.java
+++ src/leetcode/techniques/DynamicProgramming/Main.java
@@ -5,5 +5,8 @@ public class Main {
         ZeroOneKnapSack knapSack = new ZeroOneKnapSack();
         int maxProfit = knapSack.knapSack(3, new int[]{1,2,4}, new int[]{6,7,8}, 3);
         System.out.println("Maximum profit in 0/1 Knapsack Problem is : " + maxProfit);
+
+        int maxProfitWithMemoization = knapSack.knapSackMemoization(3, new int[]{1,2,4}, new int[]{6,7,8}, 3);
+        System.out.println("Maximum profile in 0/1 Knapsack Problem with Memoization is : " + maxProfitWithMemoization);
     }
 }
--- src/leetcode/techniques/DynamicProgramming/ZeroOneKnapSack.java
+++ src/leetcode/techniques/DynamicProgramming/ZeroOneKnapSack.java
@@ -1,11 +1,13 @@
 package leetcode.techniques.DynamicProgramming;
 
 public class ZeroOneKnapSack {
-    public int knapSack(int W, int wt[], int val[], int n) {
+
+    // Basic Recursion Approach. Gives TLE for some test cases.
+    public int knapSack(int W, int[] wt, int[] val, int n) {
         return knapSackHelper(W, wt, val, n - 1);
     }
 
-    private int knapSackHelper(int W, int wt[], int val[], int n) {
+    private int knapSackHelper(int W, int[] wt, int[] val, int n) {
         // Base condition
         if (n < 0) {
             return 0;
@@ -18,5 +20,47 @@ public class ZeroOneKnapSack {
 
         //Either we take or leave it
         return Math.max((val[n] + knapSackHelper(W - wt[n], wt, val, n - 1)), knapSackHelper(W, wt, val, n - 1));
+    }
+
+
+    // Fixes TLE issue when memoization is applied
+
+    //Function to return max value that can be put in knapsack of capacity W.
+    public int knapSackMemoization(int W, int[] wt, int[] val, int n)
+    {
+        int[][] dp = new int[W+1][n+1];
+
+        for(int i = 0 ; i < W+1; i++)
+        {
+            for(int j = 0 ; j < n+1; j++)
+            {
+                dp[i][j] = -1;
+            }
+        }
+
+        return knapSackHelperMemoization(W, wt, val, n-1, dp);
+    }
+
+    private int knapSackHelperMemoization(int W, int[] wt, int[] val, int n, int[][] dp)
+    {
+        // Base condition
+        if(n < 0)
+        {
+            return 0;
+        }
+
+        if(dp[W][n] != -1)
+        {
+            return dp[W][n];
+        }
+
+        // Choices
+        if(wt[n] > W)
+        {
+            return dp[W][n] = knapSackHelperMemoization(W,wt,val, n-1,dp); // we cannot put that item. Its exceeding the weight.
+        }
+
+        //Either we take or leave it
+        return dp[W][n] = Math.max((val[n] + knapSackHelperMemoization(W - wt[n], wt, val, n-1,dp)), knapSackHelperMemoization(W,wt,val, n-1,dp));
     }
 }
