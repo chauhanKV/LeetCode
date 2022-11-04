571fc77ddba451780897a744ef32ad75ca18bcc4
kavitachauhan <chauhan.kavita@hotmail.com>
2022-11-04 13:28:59 +0530

    0/1 Knapsack - P1 - Subset Sum Problem - Dynamic Programming

--- src/leetcode/techniques/DynamicProgramming/Main.java
+++ src/leetcode/techniques/DynamicProgramming/Main.java
@@ -12,5 +12,9 @@ public class Main {
         ZeroOneKnapSackTopDown topDown = new ZeroOneKnapSackTopDown();
         int maxProfitTopDown = topDown.knapSack(3, new int[]{1,2,4}, new int[]{6,7,8}, 3);
         System.out.println("0/1 KnapSack with Top Down Approach : " + maxProfitTopDown);
+
+        SubSetSum sum = new SubSetSum();
+        boolean isSubsetPresent = sum.isSubsetSum(6, new int[] {3, 34, 4, 12, 5, 2}, 30);
+        System.out.println("Is Subset Present ? : " + isSubsetPresent);
     }
 }
