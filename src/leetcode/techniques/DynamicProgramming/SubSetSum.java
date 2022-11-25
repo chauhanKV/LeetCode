cc24762da334f2a9cbbba6042b02e8bddc549c07
kavitachauhan <chauhan.kavita@hotmail.com>
2022-11-25 13:11:38 +0530

    0/1 Knapsack - P1 - SubSet Sum - With Recursion and Memoization approach

--- src/leetcode/techniques/DynamicProgramming/SubSetSum.java
+++ src/leetcode/techniques/DynamicProgramming/SubSetSum.java
@@ -1,6 +1,82 @@
 package leetcode.techniques.DynamicProgramming;
 
 public class SubSetSum {
+
+    // Recursion approach 1 - Gives TLE
+
+    // with Recursion approach - 1
+    public Boolean isSubsetSumRecursion(int N, int[] arr, int sum){
+        return isSubsetSumHelper(N, arr, sum, 0, 0);
+    }
+
+    public Boolean isSubsetSumHelper(int N, int[] arr, int target, int runningSum, int index)
+    {
+        // Base Condition
+        if(runningSum == target)
+        {
+            return true;
+        }
+
+        if(index >= arr.length)
+        {
+            return false;
+        }
+
+        return isSubsetSumHelper(N, arr, target, runningSum + arr[index], index + 1) || isSubsetSumHelper(N, arr, target, runningSum, index + 1);
+
+    }
+
+    // Memoization approach - 2
+    public Boolean isSubsetSumMemoization(int N, int[] arr, int sum){
+
+        int range = 0;
+        for (int k : arr) {
+            range += k;
+        }
+
+        int[][] dp = new int[N+1][range+1];
+
+        // initialize dp with -1
+        for(int i = 0 ; i < N + 1; i++)
+        {
+            for(int j = 0 ; j < range + 1; j++)
+            {
+                dp[i][j] = -1;
+            }
+        }
+
+        return isSubsetSumHelperMemoization(N, arr, sum, 0, 0, dp);
+    }
+
+    public Boolean isSubsetSumHelperMemoization(int N, int[] arr, int target, int runningSum, int index, int[][] dp)
+    {
+        // Base Condition
+        if(runningSum == target)
+        {
+            return true;
+        }
+
+        if(index >= arr.length)
+        {
+            return false;
+        }
+
+        if(dp[index][runningSum] != -1)
+        {
+            return dp[index][runningSum] == 1;
+        }
+
+        boolean result = isSubsetSumHelperMemoization(N, arr, target, runningSum + arr[index], index + 1, dp)
+                            ||
+                         isSubsetSumHelperMemoization(N, arr, target, runningSum, index + 1, dp);
+
+        dp[index][runningSum] = result ? 1 : 0;
+        return result;
+
+    }
+
+
+    // DP approach 3 - Optimized Approach
     public Boolean isSubsetSum(int N, int[] arr, int sum) {
 
         // Understand Knapsack basic problem to understand this.
@@ -35,4 +111,8 @@ public class SubSetSum {
 
         return t[N][sum];
     }
+
+
+
+
 }
