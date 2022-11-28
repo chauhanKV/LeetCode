c1cc612e018ce0c846f4ff120ea5c6a842f6ccdb
kavitachauhan <chauhan.kavita@hotmail.com>
2022-11-28 09:11:44 +0530

    0/1 Knapsack - P2 - Equal Sum Partition - With Recursion and Memoization approach

--- src/leetcode/techniques/DynamicProgramming/EqualSumPartition.java
+++ src/leetcode/techniques/DynamicProgramming/EqualSumPartition.java
@@ -2,6 +2,91 @@ package leetcode.techniques.DynamicProgramming;
 
 public class EqualSumPartition {
 
+
+    // Recursive Approach 1 : Gives TLE
+    public boolean canPartitionRecursive(int[] nums) {
+        int sum = 0;
+        int N = nums.length;
+
+        // Get sum of all the values in nums
+        for (int num : nums) {
+            sum += num;
+        }
+
+        if (sum % 2 != 0) {
+            return false;
+        }
+
+        return isSubsetSumHelperRecursive(nums.length, nums, sum / 2, 0, 0);
+    }
+
+    public Boolean isSubsetSumHelperRecursive(int N, int[] arr, int target, int runningSum, int index) {
+        // Base Condition
+        if (runningSum == target) {
+            return true;
+        }
+
+        if (index >= arr.length) {
+            return false;
+        }
+
+        boolean result = isSubsetSumHelperRecursive(N, arr, target, runningSum + arr[index], index + 1)
+                ||
+                isSubsetSumHelperRecursive(N, arr, target, runningSum, index + 1);
+
+        return result;
+    }
+
+
+    // Memoization Approach 2 : Takes more time than DP.
+    public boolean canPartitionMemoization(int[] nums) {
+        int sum = 0;
+        int N = nums.length;
+
+        // Get sum of all the values in nums
+        for (int num : nums) {
+            sum += num;
+        }
+
+        if (sum % 2 != 0) {
+            return false;
+        }
+
+        int[][] dp = new int[N + 1][sum + 1];
+        for (int i = 0; i < N + 1; i++) {
+            for (int j = 0; j < sum + 1; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        return isSubsetSumHelperMemoization(nums.length, nums, sum / 2, 0, 0, dp);
+    }
+
+    public Boolean isSubsetSumHelperMemoization(int N, int[] arr, int target, int runningSum, int index, int[][] dp) {
+        // Base Condition
+        if (runningSum == target) {
+            return true;
+        }
+
+        if (index >= arr.length) {
+            return false;
+        }
+
+        if (dp[index][runningSum] != -1) {
+            return dp[index][runningSum] == 1;
+        }
+
+        boolean result = isSubsetSumHelperMemoization(N, arr, target, runningSum + arr[index], index + 1, dp)
+                ||
+                isSubsetSumHelperMemoization(N, arr, target, runningSum, index + 1, dp);
+
+        dp[index][runningSum] = result ? 1 : 0;
+        return result;
+    }
+
+
+    // Dynamic Programming Approach 3 : Best Optimized Approach
+
     //417. Pacific Atlantic Water Flow
     //More challenges
     //698. Partition to K Equal Sum Subsets
