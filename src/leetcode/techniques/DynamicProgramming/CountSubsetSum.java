86f2520eec3a2cb6f350c18d0f6a6e29f3ce02c5
kavitachauhan <chauhan.kavita@hotmail.com>
2022-12-02 12:54:32 +0530

    0/1 Knapsack - P3 - Count of Subset Sum Equal to Given sum - With Recursion and Memoization approach

--- src/leetcode/techniques/DynamicProgramming/CountSubsetSum.java
+++ src/leetcode/techniques/DynamicProgramming/CountSubsetSum.java
@@ -1,6 +1,72 @@
 package leetcode.techniques.DynamicProgramming;
 
 public class CountSubsetSum {
+
+    // Approach 1 : Recursive Approach
+    public int perfectSumRecursion(int[] arr,int n, int sum) {
+        return perfectSumRecursionHelper(arr, n, sum, 0, 0);
+    }
+
+    int perfectSumRecursionHelper(int[] arr, int n, int target, int runningSum, int index) {
+        // base condition
+        if (runningSum == target) {
+            return 1;
+        }
+
+        if (index >= arr.length) {
+            return 0;
+        }
+
+        if (runningSum > target) {
+            return 0;
+        }
+
+        return perfectSumRecursionHelper(arr, n, target, runningSum + arr[index], index + 1) +
+                perfectSumRecursionHelper(arr, n, target, runningSum, index + 1);
+    }
+
+    // Approach 2 : Memoization
+    public int perfectSumMemoization(int[] arr,int n, int sum) {
+        int range = 0;
+
+        for (int k : arr) {
+            range += k;
+        }
+
+        int[][] dp = new int[n + 1][range + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < range + 1; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        return perfectSumMemoizationHelper(arr, n, sum, 0, 0, dp);
+    }
+
+    int perfectSumMemoizationHelper(int[] arr, int n, int target, int runningSum, int index, int[][] dp) {
+        // base condition
+        if (runningSum == target) {
+            return 1;
+        }
+
+        if (index >= arr.length) {
+            return 0;
+        }
+
+        if (runningSum > target) {
+            return 0;
+        }
+
+        if (dp[index][runningSum] > -1) {
+            return dp[index][runningSum];
+        }
+
+        return perfectSumMemoizationHelper(arr, n, target, runningSum + arr[index], index + 1, dp) +
+                perfectSumMemoizationHelper(arr, n, target, runningSum, index + 1, dp);
+    }
+
+    // Approach 3 : Using Dynamic Programming
     int value = 1000000007;
     public int perfectSum(int[] arr ,int n, int sum) {
 
@@ -28,7 +94,6 @@ public class CountSubsetSum {
                 }
             }
         }
-
         return t[n][sum];
     }
 }
