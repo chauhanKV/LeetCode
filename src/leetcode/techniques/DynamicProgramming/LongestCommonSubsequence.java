574c0621813e3ce5ef698a9a9cff9e80866c82d0
kavitachauhan <chauhan.kavita@hotmail.com>
2023-02-23 09:52:07 +0530

    1143. Longest Common Subsequence - With Memoization - Dynamic Programming

--- src/leetcode/techniques/DynamicProgramming/LongestCommonSubsequence.java
+++ src/leetcode/techniques/DynamicProgramming/LongestCommonSubsequence.java
@@ -2,7 +2,17 @@ package leetcode.techniques.DynamicProgramming;
 
 public class LongestCommonSubsequence {
     public int longestCommonSubsequence(String text1, String text2) {
-        return longestCommonSubsequenceRecursion(text1, text2, text1.length(), text2.length());
+        //return longestCommonSubsequenceRecursion(text1, text2, text1.length(), text2.length());
+
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        for(int i = 0 ; i < text1.length() + 1; i++)
+        {
+            for(int j = 0 ; j < text2.length() + 1; j++)
+            {
+                dp[i][j] = -1;
+            }
+        }
+        return longestCommonSubsequenceMem(text1, text2, text1.length(), text2.length(), dp);
     }
 
     public int longestCommonSubsequenceRecursion(String text1, String text2, int n, int m) {
@@ -22,6 +32,31 @@ public class LongestCommonSubsequence {
 
         // Find the max of both conditions.
         return Math.max(longestCommonSubsequenceRecursion(text1, text2, n, m - 1), longestCommonSubsequenceRecursion(text1, text2, n - 1, m));
+
+    }
+
+    // Memoized code - Approach 2
+    public int longestCommonSubsequenceMem(String text1, String text2, int n, int m, int[][] dp) {
+        // Base condition
+        // Think of the smallest valid input
+        // smallest valid input is when text1 and text2 length is 0 , so the longest common subsequence is 0 which is valid
+        if (n == 0 || m == 0)
+            return 0;  // This is because if one text value is empty there wont be any longest subsequence as nothing is common.
+
+
+        if (dp[n][m] != -1) {
+            return dp[n][m];
+        }
+        // Choice diagram
+        // If the last string is common for both text1 and text2
+
+        if (text1.charAt(n - 1) == text2.charAt(m - 1)) {
+            // We need to add 1 add we got one common string
+            return dp[n][m] = 1 + longestCommonSubsequenceMem(text1, text2, n - 1, m - 1, dp);
+        }
+
+        // Find the max of both conditions.
+        return dp[n][m] = Math.max(longestCommonSubsequenceMem(text1, text2, n, m - 1, dp), longestCommonSubsequenceMem(text1, text2, n - 1, m, dp));
 
     }
 }
