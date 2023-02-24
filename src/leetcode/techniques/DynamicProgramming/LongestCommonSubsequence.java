8d676cfadb5412b241ab3d84354f2b9f396c05d3
kavitachauhan <chauhan.kavita@hotmail.com>
2023-02-24 09:51:29 +0530

    1143. Longest Common Subsequence - With DP / Top Down - Dynamic Programming

--- src/leetcode/techniques/DynamicProgramming/LongestCommonSubsequence.java
+++ src/leetcode/techniques/DynamicProgramming/LongestCommonSubsequence.java
@@ -4,17 +4,20 @@ public class LongestCommonSubsequence {
     public int longestCommonSubsequence(String text1, String text2) {
         //return longestCommonSubsequenceRecursion(text1, text2, text1.length(), text2.length());
 
-        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
-        for(int i = 0 ; i < text1.length() + 1; i++)
-        {
-            for(int j = 0 ; j < text2.length() + 1; j++)
-            {
-                dp[i][j] = -1;
-            }
-        }
-        return longestCommonSubsequenceMem(text1, text2, text1.length(), text2.length(), dp);
+//        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+//        for(int i = 0 ; i < text1.length() + 1; i++)
+//        {
+//            for(int j = 0 ; j < text2.length() + 1; j++)
+//            {
+//                dp[i][j] = -1;
+//            }
+//        }
+//        return longestCommonSubsequenceMem(text1, text2, text1.length(), text2.length(), dp);
+
+        return longestCommonSubsequenceDP(text1, text2, text1.length(), text2.length());
     }
 
+    // Approach 1 - Recursion
     public int longestCommonSubsequenceRecursion(String text1, String text2, int n, int m) {
         // Base condition
         // Think of the smallest valid input
@@ -59,4 +62,34 @@ public class LongestCommonSubsequence {
         return dp[n][m] = Math.max(longestCommonSubsequenceMem(text1, text2, n, m - 1, dp), longestCommonSubsequenceMem(text1, text2, n - 1, m, dp));
 
     }
+
+    // Approach 3 - DP
+
+    public int longestCommonSubsequenceDP(String text1, String text2, int n, int m) {
+        int[][] dp = new int[m + 1][n + 1];
+        // Recursive base condition changes to initialization in Top down - DP approach
+        for (int i = 0; i < m + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+
+        // Choice diagram
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (text1.charAt(i) == text2.charAt(j))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+
+            }
+        }
+
+        return dp[m][n];
+    }
+
+
 }
