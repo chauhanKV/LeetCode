cab3d1304d6abadbfbdd8b7e1b78110ee467a75d
kavitachauhan <chauhan.kavita@hotmail.com>
2023-04-24 15:00:29 +0530

    132. Palindrome Partitioning II | Recursive MCM approach + Memoization + Optimized

--- src/leetcode/techniques/DynamicProgramming/PalindromePartitioningII.java
+++ src/leetcode/techniques/DynamicProgramming/PalindromePartitioningII.java
@@ -8,47 +8,16 @@ public class PalindromePartitioningII {
         //return minCutMCMRecursiveApproach(s, 0, s.length() - 1);
 
         // Dynamic Programming approach
-        for(int i = 0 ; i < s.length() + 1; i++)
-        {
-            for(int j = 0 ; j < s.length() + 1; j++)
-            {
+        for (int i = 0; i < s.length() + 1; i++) {
+            for (int j = 0; j < s.length() + 1; j++) {
                 dp[i][j] = -1;
             }
         }
 
-        return minCutRecursionAndMemoization(s, 0, s.length() - 1, dp);
-    }
+        //return minCutRecursionAndMemoization(s, 0, s.length() - 1, dp);
 
-    // MCM - Memoization Approach
-    public int minCutRecursionAndMemoization(String s, int i, int j, int[][] dp)
-    {
-        // Base Condition
-        if(i >= j)
-        {
-            return 0;
-        }
-
-        // When its palindrome - I do not have to partition so return 0;
-        if(isPalindrome(s, i, j))
-        {
-            return 0;
-        }
-
-        if(dp[i][j] != -1)
-        {
-            return dp[i][j];
-        }
-
-        // Schemes for i and j
-        int minCut = Integer.MAX_VALUE;
-        for(int k = i; k <= j-1 ; k++)
-        {
-            // Matrix chain multiplication
-            int tempAns = 1 + minCutRecursionAndMemoization(s, i, k, dp) + minCutRecursionAndMemoization(s, k+1, j, dp);
-            minCut = Math.min(tempAns, minCut);
-        }
-
-        return dp[i][j] = minCut;
+        // Dynamic Programming Optimized Approach
+        return minCutRecursionAndMemoizationOptimized(s, 0, s.length() - 1, dp);
     }
 
     public int minCutMCMRecursiveApproach(String s, int i, int j) {
@@ -71,14 +40,82 @@ public class PalindromePartitioningII {
         return minMN;
     }
 
+    // MCM - Memoization Approach
+    public int minCutRecursionAndMemoization(String s, int i, int j, int[][] dp) {
+        // Base Condition
+        if (i >= j) {
+            return 0;
+        }
+
+        // When its palindrome - I do not have to partition so return 0;
+        if (isPalindrome(s, i, j)) {
+            return 0;
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        // Schemes for i and j
+        int minCut = Integer.MAX_VALUE;
+        for (int k = i; k <= j - 1; k++) {
+            // Matrix chain multiplication
+            int tempAns = 1 + minCutRecursionAndMemoization(s, i, k, dp) + minCutRecursionAndMemoization(s, k + 1, j, dp);
+            minCut = Math.min(tempAns, minCut);
+        }
+
+        return dp[i][j] = minCut;
+    }
+
+    public int minCutRecursionAndMemoizationOptimized(String s, int i, int j, int[][] dp) {
+        // Base condition
+        if (i >= j) {
+            return 0;
+        }
+
+        if (isPalindrome(s, i, j)) {
+            return 0;
+        }
+
+        // MCM
+        int minCut = Integer.MAX_VALUE;
+        for (int k = i; k <= j - 1; k++) {
+            // left
+            int left = 0;
+            if (dp[i][k] != -1) {
+                left = dp[i][k];
+            } else {
+                left = minCutRecursionAndMemoizationOptimized(s, i, k, dp);
+                dp[i][k] = left;
+            }
+
+            // right
+            int right = 0;
+            if (dp[k + 1][j] != -1) {
+                right = dp[k + 1][j];
+            } else {
+                right = minCutRecursionAndMemoizationOptimized(s, k + 1, j, dp);
+                dp[k + 1][j] = right;
+            }
+
+            int result = 1 + left + right;
+            minCut = Math.min(minCut, result);
+        }
+
+        return dp[i][j] = minCut;
+    }
+
     public boolean isPalindrome(String s, int start, int end) {
         while (start <= end) {
             if (s.charAt(start) != s.charAt(end))
                 return false;
+
             start++;
             end--;
         }
+
         return true;
     }
+
 
 }
